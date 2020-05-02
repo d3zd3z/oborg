0ad8533a33e364765f31ab7bd169a1b65ce3fdd9
David Brown <davidb@davidb.org>
2020-05-02 16:25:12 +0000

    Create a basic test framework.
    Setup dune and the Makefile to be able to run a test framework.
    
    FossilOrigin-Name: b83a6352b9cbdfe6d9ee10c65b754a750540f574db181448c9d48fb892e1d797

--- Makefile
+++ Makefile
@@ -2,3 +2,7 @@ all:
 	dune build
 clean:
 	dune clean
+test:
+	dune runtest
+
+.PHONY: test all clean
---
+++ test/test.ml
@@ -0,0 +1,5 @@
+(* Borg interaction testing. *)
+
+open Core
+
+let () = printf "Tests would be here\n"
